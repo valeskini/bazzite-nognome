29e1c3ce3bae0cd0939ec48a3fe0135a2c2e9593
Massi Valeskini <massi@valeskini.dev>
2025-12-09 15:46:54 +0100

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,6 @@ dnf5 install -y plasma-discover \
   plasma-discover-flatpak \
   plasma-discover-notifier \
   plasma-discover-kns \
-  kde-partitionmanager \
   --exclude=plasma-discover-offline-updates,plasma-discover-packagekit,plasma-discover-rpm-ostree,packagekit
 
 dnf5 install -y konsole \
